a2063c0bbfb55f452348f65556e863e8453199f5
Andy Chen <andycjx8@gmail.com>
2021-08-24 08:58:25 +1000

    object desctructing props for counters

--- src/components/counters.jsx
+++ src/components/counters.jsx
@@ -1,19 +1,19 @@
 import React from "react";
 import Counter from "./counter";
 
-const Counters = (props) => {
+const Counters = ({ onReset, onIncrement, onDelete, counters }) => {
   return (
     <div>
-      <button onClick={props.onReset} className="btn-primary btn-sm m-2">
+      <button onClick={onReset} className="btn-primary btn-sm m-2">
         Reset
       </button>
       <ul>
-        {this.props.counters.map((counter) => (
+        {counters.map((counter) => (
           <li key={counter.id}>
             <Counter
               counter={counter}
-              onIncrement={props.onIncrement}
-              onDelete={props.onDelete}
+              onIncrement={onIncrement}
+              onDelete={onDelete}
             />
           </li>
         ))}
