afec4736d692114caedea2d2e856097ee23b8d9c
Andy Chen <andycjx8@gmail.com>
2021-08-24 08:51:45 +1000

    Object destructing for props

--- src/components/navbar.jsx
+++ src/components/navbar.jsx
@@ -1,14 +1,12 @@
-import React, { Component } from "react";
+import React from "react";
 
-const NavBar = (props) => {
+const NavBar = ({ totalCounters }) => {
   return (
     <div>
       <nav className="navbar navbar-light bg-light">
         <a className="navbar-brand mb-0 h1">
           Navbar{" "}
-          <span className="badge badge-pill bg-info">
-            {props.totalCounters}
-          </span>
+          <span className="badge badge-pill bg-info">{totalCounters}</span>
         </a>
       </nav>
     </div>
