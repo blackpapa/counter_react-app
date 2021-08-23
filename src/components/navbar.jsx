a48e98edd590394f5ba5d53c81c3b1317591f564
Andy Chen <andycjx8@gmail.com>
2021-08-24 08:51:01 +1000

    change from class component to stateless fc

--- src/components/navbar.jsx
+++ src/components/navbar.jsx
@@ -1,20 +1,18 @@
 import React, { Component } from "react";
 
-class NavBar extends Component {
-  render() {
-    return (
-      <div>
-        <nav className="navbar navbar-light bg-light">
-          <a className="navbar-brand mb-0 h1">
-            Navbar{" "}
-            <span className="badge badge-pill bg-info">
-              {this.props.totalCounters}
-            </span>
-          </a>
-        </nav>
-      </div>
-    );
-  }
-}
+const NavBar = (props) => {
+  return (
+    <div>
+      <nav className="navbar navbar-light bg-light">
+        <a className="navbar-brand mb-0 h1">
+          Navbar{" "}
+          <span className="badge badge-pill bg-info">
+            {props.totalCounters}
+          </span>
+        </a>
+      </nav>
+    </div>
+  );
+};
 
 export default NavBar;
