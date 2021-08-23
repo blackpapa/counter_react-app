57d5f6c87f11809fceea688275e630fb89e89271
Andy Chen <andycjx8@gmail.com>
2021-08-24 08:55:32 +1000

    Change from cc to sfc for counters

--- src/components/counters.jsx
+++ src/components/counters.jsx
@@ -1,27 +1,25 @@
-import React, { Component } from "react";
+import React from "react";
 import Counter from "./counter";
 
-class Counters extends Component {
-  render() {
-    return (
-      <div>
-        <button onClick={this.props.onReset} className="btn-primary btn-sm m-2">
-          Reset
-        </button>
-        <ul>
-          {this.props.counters.map((counter) => (
-            <li key={counter.id}>
-              <Counter
-                counter={counter}
-                onIncrement={this.props.onIncrement}
-                onDelete={this.props.onDelete}
-              />
-            </li>
-          ))}
-        </ul>
-      </div>
-    );
-  }
-}
+const Counters = (props) => {
+  return (
+    <div>
+      <button onClick={props.onReset} className="btn-primary btn-sm m-2">
+        Reset
+      </button>
+      <ul>
+        {this.props.counters.map((counter) => (
+          <li key={counter.id}>
+            <Counter
+              counter={counter}
+              onIncrement={props.onIncrement}
+              onDelete={props.onDelete}
+            />
+          </li>
+        ))}
+      </ul>
+    </div>
+  );
+};
 
 export default Counters;
